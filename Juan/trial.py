6264ad9f501d49b6045cb5f929b741f0e99255ad
Juan Esteban Agudelo Ortiz <juanessao2000@pop-os.localdomain>
2022-08-27 11:21:53 -0500

    organizing folders

---
+++ Juan/trial.py
@@ -0,0 +1,13 @@
+import ee
+
+ee.Authenticate()
+ee.Initialize()
+
+# Import the MODIS land cover collection.
+lc = ee.ImageCollection('MODIS/006/MCD12Q1')
+
+# Import the MODIS land surface temperature collection.
+lst = ee.ImageCollection('MODIS/006/MOD11A1')
+
+# Import the USGS ground elevation image.
+elv = ee.Image('USGS/SRTMGL1_003')
