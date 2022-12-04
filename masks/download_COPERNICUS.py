32a27d7aa742972d6a5a38e57153eaeb47bd0ab5
Juan Esteban Agudelo Ortiz <juanessao2000@pop-os.localdomain>
2022-12-04 13:37:33 -0500

    script for downloading our COPERNICUS data

--- masks/download_COPERNICUS.py
+++
@@ -1,6 +0,0 @@
-import gdown
-
-# a folder
-
-url = "https://drive.google.com/drive/folders/1S0KGczfzo2fpjjf-A_BLxQHlgsQllqsE?usp=share_link"
-gdown.download_folder(url, quiet = True, use_cookies=False)
--- masks/download_COPERNICUS_drive.sh
+++ masks/download_COPERNICUS_drive.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 gdown https://drive.google.com/drive/u/1/folders/1S0KGczfzo2fpjjf-A_BLxQHlgsQllqsE -O /hdd/1-PRINCIPAL-2022-1/1-PROYECTOS/PROYECTO_FREGATA_SPACE/Code/masks --folder
